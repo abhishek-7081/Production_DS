e5f8e988a3a35ffff9f5c475241abd8ff289a12c
abhishek-7081 <luckydwivedi7081@gmail.com>
2024-11-10 09:11:34 +0530

    1st Bubble sort-Basic logic

---
+++ Bubble_Sort/bs_1.java
@@ -0,0 +1,28 @@
+public class bs_1 {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 23, 11, 7, 8, 9, 5, 1 };
+
+        // time complexity O(n^2)
+        // bubble sort
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+        printArray(arr);
+    }
+}
