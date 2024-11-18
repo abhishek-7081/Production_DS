d56c69f17f41b4cc2c053336e6de5ed158ba52f7
abhishek-7081 <luckydwivedi7081@gmail.com>
2024-11-18 14:59:25 +0530

    2d arrays basics and finding x

---
+++ Arrays/_2d_ar_find_x_.java
@@ -0,0 +1,56 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class _2d_ar_find_x_ {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+        int[][] arr = new int[rows][columns];
+
+        // input
+        // rows
+        for (int i = 0; i < rows; i++) {
+            // columns
+            for (int j = 0; j < columns; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+
+        // output
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (arr[i][j] == x) {
+                    System.out.println("the no is at " + i + ", " + j);
+
+                }
+            }
+
+        }
+
+    //     boolean found = false; // Flag to track if the number is found
+
+    //     for (int i = 0; i < rows; i++) {
+    //         for (int j = 0; j < columns; j++) {
+    //             if (arr[i][j] == x) {
+    //                 System.out.println("The number is at " + i + ", " + j);
+    //                 found = true; // Update the flag
+    //                 break; // Exit the inner loop
+    //             }
+    //         }
+    //         if (found) {
+    //             break; // Exit the outer loop if the number is found
+    //         }
+    //     }
+
+    //     if (!found) {
+    //         System.out.println("The number is not found in the array.");
+    //     }
+
+     }
+
+}
---
+++ Arrays/_2d_array_in_out_.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.*;
+
+public class _2d_array_in_out_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+        int[][] arr = new int[rows][columns];
+
+        // input
+        // rows
+        for (int i = 0; i < rows; i++) {
+            // columns
+            for (int j = 0; j < columns; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        // output
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
