88546139c2114132dc1ffb657d32746ff40e97df
abhishek-7081 <luckydwivedi7081@gmail.com>
2024-11-18 15:13:30 +0530

    String length and char

---
+++ String/st1.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.util.Scanner;
+
+public class st1 {
+    public static void main(String[] args) {
+        Scanner sb = new Scanner(System.in);
+        String s = sb.nextLine();
+
+        // for length
+        System.out.println("The length of string is: "+s.length());
+
+        // for every char print
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+
+    }
+}
