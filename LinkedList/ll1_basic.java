3bfa6d3c51ddca3a0807082f4cf4e8d2401ef7e4
abhishek-7081 <luckydwivedi7081@gmail.com>
2024-11-19 20:35:25 +0530

    about collection framework

--- LinkedList/ll1_basic.java
+++ LinkedList/ll1_basic.java
@@ -1,13 +1,20 @@
 package LinkedList;
 
+
+//collection framework
+
 import java.util.LinkedList;
 
 public class ll1_basic {
 
     public static void main(String args[]) {
+
+       //making linked list
+
         LinkedList<String> list = new LinkedList<String>();
         list.add("is");
         list.add("a");
+        
         list.addLast("list");
         list.addFirst("this");
         System.out.println(list);
