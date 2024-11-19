0f4b37d71689e5518ae209ee89efe623b24198bb
abhishek-7081 <luckydwivedi7081@gmail.com>
2024-11-19 19:41:37 +0530

    normal linked list creation and their functions

---
+++ LinkedList/ll1_basic.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class ll1_basic {
+
+    public static void main(String args[]) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("is");
+        list.add("a");
+        list.addLast("list");
+        list.addFirst("this");
+        System.out.println(list);
+
+        list.add(3, "linked");
+        System.out.println(list);
+
+        System.out.println(list.get(0));
+        System.out.println(list.size());
+        list.remove(3);
+
+        System.out.println(list);
+        list.removeFirst();
+
+        System.out.println(list);
+        list.removeLast();
+
+        System.out.println(list);
+    }
+
+}
