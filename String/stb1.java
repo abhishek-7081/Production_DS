9e5b33c2f27c73452d0d93e8ecad1bc9f5bfe941
abhishek-7081 <luckydwivedi7081@gmail.com>
2024-11-19 07:42:40 +0530

    added time comp

--- String/stb1.java
+++ String/stb1.java
@@ -2,9 +2,12 @@
 
 public class stb1 {
     public static void main(String[] args) {
+
+        // Time compexity O(n)
+
         StringBuilder sb = new StringBuilder("Hello World");
         System.out.println("String = " + sb);
-        
+
         // Reversing the StringBuilder
         for (int i = 0; i < sb.length() / 2; i++) {
             int front = i;
