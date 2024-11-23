08c676879a025ddba667db30c916bc3a16224f7b
abhishek-7081 <luckydwivedi7081@gmail.com>
2024-11-23 12:38:58 +0530

    Class FrameWork

---
+++ Stack/sta3_cla_famework.java
@@ -0,0 +1,14 @@
+package Stack;
+import java.util.*;
+
+public class sta3_cla_famework {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        System.out.println("Stack: " + s);
+        }
+}
