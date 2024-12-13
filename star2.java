0e8cd2618c154029a88454db4a90d01e4a41854e
abhishek-7081 <luckydwivedi7081@gmail.com>
2024-12-13 16:17:43 +0530

    1 22 333 4444 55555 pattern

---
+++ star2.java
@@ -0,0 +1,14 @@
+public class star2 {
+
+    public static void main(String[] args) {
+        int n = 5; 
+       // Outer loop for each row
+        for (int i = 1; i <= n; i++) {
+        // Inner loop for printing numbers in each row
+        for (int j = 1; j <= i; j++) {
+        System.out.print(i); 
+       }
+        System.out.println(); }
+        }
+    
+}
